46d2f729e81c1edf13351f7c121f041343418556
Mahmoud El-Najar <31566070+moud449@users.noreply.github.com>
2021-12-02 07:36:07 +0200

    update config.php

--- config.php
+++ config.php
@@ -12,16 +12,10 @@
 return [
   
     // Add you bot's API key (that you get from BotFather )
-    'api_key'      => '1908306081:AAH2-HY34mPVAJZPAlyS_Y_YFUB14ebqbBc',
-
-    // When using the getUpdates method, this can be commented out
+    'api_key'      => '1908306081:AAH2-HY3mPVAJZPAlyS_Y_YFUB14ebqbBc',
     'webhook'      => [
-        //  'url' => 'https://4e6008339667.ngrok.io/elbot/hook.php',
-        'url' => 'https://ola-gam3a.com/elbot/hook.php',
-        // Use self-signed certificate
-        // 'certificate'     => __DIR__ . '/path/to/your/certificate.crt',
-        // Limit maximum number of connections
-        // 'max_connections' => 5,
+        'url' => 'https://....../hook.php',
+
     ],
     
 ];
